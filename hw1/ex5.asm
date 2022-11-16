bbc5e229700203878423debc545314fd086bf3a2
Yuval Lande <yuvalande@gmail.com>
2022-11-16 18:58:21 +0000

    finished ex4

--- hw1/ex5.asm
+++ hw1/ex5.asm
@@ -2,4 +2,19 @@
 
 .section .text
 _start:
-#your code here
+
+movq head, %rax # rax is node*
+cmpq %rax, $0
+je end_HW1
+movl value, %r8d
+movq $0, %rcx # rcx is counter of nodes with value
+
+# while loop to find two nodes with value
+loop1_HW1:
+  movl (%rax), %r9d # tmp value
+  cmpl %r8d, %r9d
+  
+
+
+
+end_HW1:
