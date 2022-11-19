f2fb34d358e4e46f13070e600ab7b333ef8ff0ce
Yuval Lande <yuvalande@gmail.com>
2022-11-19 19:36:33 +0200

    ex2: added overlap support for positive input

--- hw1/ex2.asm
+++ hw1/ex2.asm
@@ -3,21 +3,30 @@
 .section .text
 _start:
 
-  movl $0x0, %edi # dest index
-  movl $0x0, %esi # src index
+  movq $0x0, %rdi # dest index
+  movq $0x0, %rsi # src index
   movl (num), %eax # RAX = n
   # check if num is negative
   cdq
   testl %edx, %edx
   jnz NEG_HW1
 
+  # check if src < des <= src+n jump to overlap_pos
+  movq $destination, %r15
+  cmpq $source, %r15
+  jle pos_loop_HW1
+  movq $source, %r14
+  movslq %eax, %rax
+  addq %rax, %r14 # r14 is $src + n
+  cmpq %r15, %r14
+  jge overlap_pos
+
 pos_loop_HW1:
   # move source+offset to destination+offset
-  movb source(%esi), %cl
-  movb %cl, destination(%edi)
+  movb source(%rsi), %cl
+  movb %cl, destination(%rsi)
   # offset++
-  inc %esi
-  inc %edi
+  inc %rsi
 
   dec %eax # counter --
   # jump to loop if counter>0 else jump to end
@@ -25,17 +34,33 @@ pos_loop_HW1:
   jnz pos_loop_HW1
   jmp end_HW1
 
+overlap_pos:
+  movq %rax, %rsi # rsi = n
+  dec %rsi
+  pos_loop_overlap_HW1:
+    # move source+offset to destination+offset
+    movb source(%rsi), %cl
+    movb %cl, destination(%rsi)
+    # offset--
+    dec %rsi
+
+    dec %eax # counter --
+    # jump to loop if counter>0 else jump to end
+    testl %eax, %eax
+    jnz pos_loop_overlap_HW1
+    jmp end_HW1
+
 NEG_HW1:
   notl %eax
-  movl %eax, %edi
+  movslq %eax, %rdi
   inc %eax
   neg_loop_HW1:
     # move source+offset to destination+offset
-    movb source(%esi), %cl
-    movb %cl, destination(%edi)
+    movb source(%rsi), %cl
+    movb %cl, destination(%rdi)
     # offset changes
-    inc %esi
-    dec %edi
+    inc %rsi
+    dec %rdi
 
     dec %eax # counter --
     # jump to loop if counter>0 else jump to end
