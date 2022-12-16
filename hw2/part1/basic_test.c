6725a3644f5154b7c6a3c885b18047003c580467
Yuval Lande <yuvalande@gmail.com>
2022-12-16 15:52:07 +0200

    passed tests

--- hw2/part1/basic_test.c
+++ hw2/part1/basic_test.c
@@ -1,10 +1,15 @@
 #include<stdio.h>
+#include<assert.h>
 
 unsigned short count_above(char separator,long limit);
 
 int main() {
-	printf("%hi\n",count_above(',', -10));
-	printf("%hi\n",count_above(',', -100));
-	printf("%hi\n",count_above(',', -1000));
+	short res = count_above('_', 94);
+	if(res != 0)	{
+		printf("Test 33 failed:\n");
+		printf("	Test 33 output: %hi\n",res);
+		printf("	Test 33 expected: 0\n");
+	}
+	else printf("Test 33 passed\n");
 	return 0;
 }
