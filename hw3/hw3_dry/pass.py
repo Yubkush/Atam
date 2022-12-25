90701d096f1a7386f608d36aaf4c5c0161f55481
Yuval Lande <yuvalande@gmail.com>
2022-12-25 14:56:36 +0200

    changed python script

--- hw3/hw3_dry/pass.py
+++ hw3/hw3_dry/pass.py
@@ -1,10 +1,9 @@
 hash = 0x939f103
 password = ""
-length = 6
 
-for i in range(length):
-    letter = (hash // (26**(length-1-i))) + ord('a')
+while(hash > 0):
+    letter = (hash % 26) + ord('a')
     password += chr(letter)
-    hash -= (letter - ord('a'))*(26**(length-1-i))
+    hash //= 26
 
-print(password)
+print(password[::-1])
